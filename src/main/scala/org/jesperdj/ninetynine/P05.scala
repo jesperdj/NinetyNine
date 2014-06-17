a0f1420e675d2b49c958d94a9dbc88fa9f804815
Jesper de Jong <jespdj@gmail.com>
2014-06-17 23:50:21 +0200

    Added P06.

--- src/main/scala/org/jesperdj/ninetynine/P05.scala
+++ src/main/scala/org/jesperdj/ninetynine/P05.scala
@@ -4,7 +4,7 @@ object P05 {
 
   def reverse[T](xs: List[T]): List[T] = xs match {
     case Nil => Nil
-    case x :: Nil => List(x)
+    case _ :: Nil => xs
     case x :: tail => reverse(tail) :+ x
   }
 }
