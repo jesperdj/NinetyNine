378458401276a791c7e6018748aaad0569ed924c
Jesper de Jong <jespdj@gmail.com>
2014-06-19 22:19:19 +0200

    Implemented P08.

--- src/main/scala/org/jesperdj/ninetynine/lists/P08.scala
+++ src/main/scala/org/jesperdj/ninetynine/lists/P08.scala
@@ -2,5 +2,10 @@ package org.jesperdj.ninetynine.lists
 
 object P08 {
 
-  def compress[T](xs: List[T]): List[T] = ???
+  def compress[T](xs: List[T]): List[T] = xs match {
+    case x1 :: x2 :: tail if x1 == x2 => compress(x2 :: tail)
+    case x1 :: x2 :: tail if x1 != x2 => x1 :: compress(x2 :: tail)
+    case _ :: Nil => xs
+    case Nil => Nil
+  }
 }
