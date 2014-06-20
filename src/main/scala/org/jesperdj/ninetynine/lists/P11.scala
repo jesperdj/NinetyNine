1e238a27498b5d95ca6f8323df18c43c41fa84f9
Jesper de Jong <jespdj@gmail.com>
2014-06-20 23:23:03 +0200

    Implemented P11.

--- src/main/scala/org/jesperdj/ninetynine/lists/P11.scala
+++ src/main/scala/org/jesperdj/ninetynine/lists/P11.scala
@@ -1,6 +1,10 @@
 package org.jesperdj.ninetynine.lists
 
 object P11 {
+  import P10._
 
-  def encodeModified[T](xs: List[T]): List[Any] = ???
+  def encodeModified[T](xs: List[T]): List[Any] = encode(xs) map {
+    case (1, x) => x
+    case e => e
+  }
 }
