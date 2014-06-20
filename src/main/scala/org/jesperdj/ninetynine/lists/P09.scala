5dd82020910d741e4d3deb7b93c240f14a80af1f
Jesper de Jong <jespdj@gmail.com>
2014-06-20 23:46:35 +0200

    Minor improvements to P09. Implemented P13.

--- src/main/scala/org/jesperdj/ninetynine/lists/P09.scala
+++ src/main/scala/org/jesperdj/ninetynine/lists/P09.scala
@@ -3,12 +3,12 @@ package org.jesperdj.ninetynine.lists
 object P09 {
 
   def pack[T](xs: List[T]): List[List[T]] = xs match {
-    case x1 :: x2 :: tail if x1 == x2 =>
-      val rest = pack(x2 :: tail)
-      (x1 :: rest.head) :: rest.tail
+    case x1 :: x2 :: tail if x1 == x2 => pack(x2 :: tail) match {
+      case h :: t => (x1 :: h) :: t
+      case Nil => Nil
+    }
 
-    case x1 :: x2 :: tail if x1 != x2 =>
-      List(x1) :: pack(x2 :: tail)
+    case x1 :: x2 :: tail if x1 != x2 => List(x1) :: pack(x2 :: tail)
 
     case _ :: Nil => List(xs)
 
--- src/main/scala/org/jesperdj/ninetynine/lists/P13.scala
+++ src/main/scala/org/jesperdj/ninetynine/lists/P13.scala
@@ -2,5 +2,16 @@ package org.jesperdj.ninetynine.lists
 
 object P13 {
 
-  def encodeDirect[T](xs: List[T]): List[(Int, T)] = ???
+  def encodeDirect[T](xs: List[T]): List[(Int, T)] = xs match {
+    case x1 :: x2 :: tail if x1 == x2 => encodeDirect(x2 :: tail) match {
+      case (c, x) :: t => (c + 1, x) :: t
+      case Nil => Nil
+    }
+
+    case x1 :: x2 :: tail if x1 != x2 => (1, x1) :: encodeDirect(x2 :: tail)
+
+    case x :: Nil => List((1, x))
+
+    case Nil => Nil
+  }
 }
