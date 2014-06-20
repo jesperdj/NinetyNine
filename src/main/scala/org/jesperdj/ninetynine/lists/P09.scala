0f52cc221c17b7b4f6c6ebf0ccf486d753285976
Jesper de Jong <jespdj@gmail.com>
2014-06-20 23:11:30 +0200

    Implemented P09.

--- src/main/scala/org/jesperdj/ninetynine/lists/P09.scala
+++ src/main/scala/org/jesperdj/ninetynine/lists/P09.scala
@@ -2,5 +2,16 @@ package org.jesperdj.ninetynine.lists
 
 object P09 {
 
-  def pack[T](xs: List[T]): List[List[T]] = ???
+  def pack[T](xs: List[T]): List[List[T]] = xs match {
+    case x1 :: x2 :: tail if x1 == x2 =>
+      val rest = pack(x2 :: tail)
+      (x1 :: rest.head) :: rest.tail
+
+    case x1 :: x2 :: tail if x1 != x2 =>
+      List(x1) :: pack(x2 :: tail)
+
+    case x :: Nil => List(List(x))
+
+    case Nil => Nil
+  }
 }
