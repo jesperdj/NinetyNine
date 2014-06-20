e823bff3d18f54d743c490a67167df09b3ba57e4
Jesper de Jong <jespdj@gmail.com>
2014-06-20 23:27:51 +0200

    Implemented P12.

--- src/main/scala/org/jesperdj/ninetynine/lists/P12.scala
+++ src/main/scala/org/jesperdj/ninetynine/lists/P12.scala
@@ -2,5 +2,5 @@ package org.jesperdj.ninetynine.lists
 
 object P12 {
 
-  def decode[T](xs: List[(Int, T)]): List[T] = ???
+  def decode[T](xs: List[(Int, T)]): List[T] = xs flatMap { case (c, x) => for (i <- 1 to c) yield x }
 }
